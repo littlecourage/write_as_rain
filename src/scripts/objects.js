3c1de693d66ee0e5b01de9987fc7fcabdfcdebfa
littlecourage <christine.adams21@gmail.com>
2020-12-08 09:54:14 -0500

    Added lifespan to each snowflakes to control number of flakes

--- src/scripts/objects.js
+++ src/scripts/objects.js
@@ -32,7 +32,7 @@ class Snowflake extends FallingObject {
     this.startY = posY;
     this.startX = posX;
     this.initialangle = Math.random() * (Math.PI * 2);
-
+    this.lifespan = 350;
     // radius of snowflake spiral
     // chosen so the snowflakes are uniformly spread out in area
     let windowWidth = this.ctx.width
@@ -42,24 +42,32 @@ class Snowflake extends FallingObject {
   }
 
   update(time) {
-    let change = 0.2; // angular speed
-    let angle = change * time + this.initialangle;
-    this.posX = (this.ctx.width / 2) + (this.radius * Math.sin(angle));
+    let spiralRate = 0.1; // angular speed
+    let spiralChange = spiralRate * time
+    let newAngle = this.initialangle + spiralChange;
+    
+    // controls how the snowflake spirals as it falls
+    this.posX = (this.ctx.width / 2) + (this.radius * Math.sin(newAngle));
 
-    if (this.posX > this.ctx.width || this.posX < 0) {
-      this.posX = this.startX;
-      this.posY = this.startY;
-    }
+    // //snowflake goes back to top if it goes too far to the side
+    // seems unnecessary
+    // if (this.posX > this.ctx.width || this.posX < 0) {
+    //   this.posX = this.startX;
+    //   this.posY = this.startY;
+    // }
 
-    // // different size snowflakes fall at slightly different y speeds
+    // different size snowflakes fall at slightly different y speeds
+    // can change the falling speed, but lifespan needs to increase accordingly
     this.posY += Math.pow(this.width, 0.5);
     if (this.posY >= this.ctx.height) {
-      //snow rests on bottom
+      //snow rests on bottom for rest of lifespan
       this.posY = this.ctx.height;
       //snow doesn't pile up
       // this.posY = this.startY;
       // this.posX = this.startX;
     }
+
+    this.lifespan --;
   }
 
   display() {
--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,17 +1,13 @@
 import p5 from 'p5';
-import collide2d from "p5.collide2d";
 import { FallingObject, Snowflake } from './objects'
 
 const sketch = (p) => {
   let snowflakes = [];
   let height = 600;
   let width = 600;
-  let blobX = p.random(0, width);
-  let blobY = Math.random() * -50;
   let color = 240;
-  let blob;
-
   let blobSpeed = 1;
+  let backgroundColor = 'gray';
 
   p.setup = () => {
     p.createCanvas(height, width);
@@ -21,24 +17,28 @@ const sketch = (p) => {
   }
   
   p.draw = () => {
-    p.background('gray');
-    for (let i = 0; i < p.random(5); i++) {
-      let blobWidth = p.random(2, 5);
-      let blobHeight = p.random(2, 5);
-      snowflakes.push(new Snowflake(blobX, blobY, p, blobWidth, blobHeight, blobSpeed, color)); // append snowflake object
-    }
-    if (snowflakes.length > 1000) {
-      snowflakes.splice(0, 100);
-      console.log(snowflakes.length);
-    }
+    p.background(backgroundColor);
+      for (let i = 0; i < p.random(5); i++) {
+        let blobX = p.random(0, width);
+        let blobY = Math.random() * -50;
+        let blobWidth = p.random(2, 5);
+        let blobHeight = p.random(2, 5);
+        snowflakes.push(new Snowflake(blobX, blobY, p, blobWidth, blobHeight, blobSpeed, color)); // append snowflake object
+      }
+    // splice off large chunk of snowflakes - was using this before chekcing lifespan
+    // if (snowflakes.length > 1000) {
+    //   snowflakes.splice(0, 100);
+    //   console.log(snowflakes.length);
+    // }
     let t = p.frameCount / 60;
-    // blob.display();
-    // blob.update(t);
-    // loop through snowflakes with a for..of loop
-    for (let flake of snowflakes) {
+    // loop through snowflakes to check their lifespan
+    for (let i = 0; i < snowflakes.length; i ++) {
+      let flake = snowflakes[i];
+      if (flake.lifespan <= 0) {
+        snowflakes.splice(i, 1)
+      }
       flake.update(t); // update snowflake position
-      // delete snowflake if past end of screen
-      flake.display(); // draw snowflake
+      flake.display(); // redraw snowflake
     }
   }
 }
