963117236c4614cb69ae9498e2189be23986d6f7
littlecourage <christine.adams21@gmail.com>
2020-12-09 14:12:56 -0500

    Copied alternative API url to weather_api but not currently using it

--- src/data/weather_api.js
+++ src/data/weather_api.js
@@ -1,5 +1,6 @@
 // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 // const targetUrl = 'https://www.metaweather.com/api/location/2459115/';
+// this.targetUrl = `https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=API_KEY`
 
 
 
@@ -7,6 +8,7 @@ class APIWeather {
 
   constructor(location) {
     this.location = location;
+    this.key = 
     this.proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     this.targetUrl = `https://www.metaweather.com/api/location/${location}/`
     this.getWeather = this.getWeather.bind(this);
